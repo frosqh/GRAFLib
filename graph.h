cf7db4f0f3e36c5a5c53f3bb424b78f4fdb63ca9
Valentin <co.merce@hotmail.fr>
2018-09-28 12:05:51 +0200

    Début menu

--- graph.h
+++ graph.h
@@ -1,19 +1,20 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+#import "liblist/list.h"
 
 #ifndef TP2GRAPH_GRAPH_H
 #define TP2GRAPH_GRAPH_H
 
-struct Neighbour {
-	int neighbour;
-	int weight;
-	Neighbour nextNeighbour;
-	Neighbour previousNeighbour;
-};
+struct Graph g;
 
 struct Graph{
 	bool isDirected;
 	int nbMaxNodes;
-	Neighbour *adjList;
 };
+
+
+void createGraph(int maxNodes, bool directed);
+
+void menu();
 
 #endif //TP2GRAPH_GRAPH_H
--- main.c
+++ main.c
@@ -1,15 +1,35 @@
-#include <stdio.h>
-
+#include <stdbool.h>
+#include <string.h>
 #import "graph.h"
 
-
 int main() {
-
-	Graph g;
-	g.isDirected = false;
-	g.nbMaxNodes = 4;
-	g.adjList = malloc(g.nbMaxNodes*sizeof(Neighbour));
-
 	printf("Hello, World!\n");
+	menu();
 	return 0;
 }
+
+void menu() {
+	char val[50] = "";
+	char val2[50] = "";
+
+	printf("===== Menu =====\n");
+	printf("1. CreateGraph \n");
+	printf("2. ... \n");
+
+	while (fgets(val, 50, stdin) && atoi(val) != 9) {
+		if (atoi(val) == 1) {
+			printf("Nb max nodes\n");
+			fgets(val, 50, stdin);
+			printf("0 No directed, 1 Directed\n");
+			fgets(val2, 50, stdin);
+			createGraph(atoi(val), atoi(val2) == 0 ? false : true);
+			printf("Graph G with %i max nodes, %sdirected created \n", g.nbMaxNodes, g.isDirected ? "" : "no ");
+		}
+	}
+}
+
+void createGraph(int maxNodes, bool directed) {
+	g.isDirected = directed;
+	g.nbMaxNodes = maxNodes;
+}
+
